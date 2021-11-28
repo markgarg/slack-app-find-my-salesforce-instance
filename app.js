d8b566212d17d464feb1f1af4cd2ba1df2988a59
Rohit Macherla <rohit.macherla@gmail.com>
2021-11-28 18:28:01 +0000

    feat: building a slash command

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
-
 // Require the Bolt package (github.com/slackapi/bolt)
 const { App } = require("@slack/bolt");
-const axios = require("axios");
+const findInstanceForAlias = require("./slashCommand");
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN 
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true,
+    appToken: process.env.SLACK_APP_TOKEN
 });
 
-
-//Add code here
-const findInstanceForAlias = async (alias) => {
-  const url = `https://api.status.salesforce.com/v1/search/${alias}`;
-  try {
-    const response = await axios.get(url);
-  } catch (error) {
-    console.error('error in findInstanceForAlias :', error);
-  }
-}
+app.command("/find-salesforce-instance", async ({ command, ack, say }) => {
+    try {
+        console.log(`Received command ${command}`);
+        await ack();
+        const instanceKey = await findInstanceForAlias(command.text);
+        say(instanceKey);
+    } catch (error) {
+        console.log("err")
+        console.error(error);
+        say("error :", error);
+    }
+});
 
 (async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
+    // Start your app
+    await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+    console.log('⚡️ Bolt app is running!');
 })();
---
+++ slashCommand.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const URL_TEMPLATE = process.env.SALESFORCE_SEARCH_URL || 'https://api.status.salesforce.com/v1/search/';
+
+const findInstanceForAlias = async (alias) => {
+    _validateCommandInput(alias);
+
+    const url = `${URL_TEMPLATE}${alias}`;
+    try {
+        const response = await axios.get(url);
+        console.log(response.data);
+        return response.data.instanceKey;
+    } catch (error) {
+        console.error('error in findInstanceForAlias :', error);
+        throw error;
+    }
+}
+
+const _validateCommandInput = (alias) => {
+    if(!alias) {
+        throw new Error('Alias is required');
+    }
+}
+
+module.exports = findInstanceForAlias;
